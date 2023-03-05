5249aac82c872afa1f2d032af068c5437dc58863
Your name here <elia.orsini@hotmail.com>
2023-03-05 18:54:26 +0000

    fix: @components/Item.tsx import

--- components/Item.tsx
+++ components/Item.tsx
@@ -1,11 +1,10 @@
-import IPrompt from "types";
+import { IPrompt } from "types";
 
 export default function Item({ content, fullData }) {
-
   const itemsWithSameId = fullData.filter((item: IPrompt) => {
     return item.id === content.id;
   });
-  
+
   return (
     <a href={`prompts/${content.id}`}>
       <div className="text-sm sm:mt-2 mt-2 mx-auto border border-black font-semibold rounded px-2 sm:w-80 w-72 hover:bg-gray-100 text-left">
