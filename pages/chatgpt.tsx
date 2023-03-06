3cb63b22d68bd514b6cd25a0f55146cf0fb7bafc
Your name here <elia.orsini@hotmail.com>
2023-03-06 19:06:35 +0000

    fix: chatGPTcustom

--- pages/chatgpt.tsx
+++ pages/chatgpt.tsx
@@ -2,10 +2,13 @@ import React, { useState } from "react";
 import axios from "axios";
 import { ChatMessage } from "types";
 import { withPasswordProtect } from "next-password-protect";
+import { Oval } from "react-loading-icons";
 
 const ChatGPT = ({ secret }) => {
   const [prompt, setPrompt] = useState<string>("");
   const [chat, setChat] = useState<Array<ChatMessage>>([]);
+
+  const [loading, setLoading] = useState<boolean>(false);
 
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>): Promise<void> => {
     event.preventDefault();
@@ -15,27 +18,34 @@ const ChatGPT = ({ secret }) => {
     const newResponse = [...chat];
     newResponse.push({ role: "user", content: prompt });
 
+    setLoading(true);
+
     try {
-      const response = await axios.post(
-        API_URL,
-        {
-          model: "gpt-3.5-turbo",
-          messages: newResponse,
-          max_tokens: 5,
-          n: 1,
-          temperature: 0.5,
-        },
-        {
-          headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${API_KEY}`,
+      axios
+        .post(
+          API_URL,
+          {
+            model: "gpt-3.5-turbo",
+            messages: newResponse,
+            max_tokens: 200,
+            n: 1,
+            temperature: 0.5,
           },
-        }
-      );
+          {
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: `Bearer ${API_KEY}`,
+            },
+          }
+        )
+        .then((response) => {
+          setLoading(false);
 
-      newResponse.push({ role: "assistant", content: response.data.choices[0].message.content });
+          newResponse.push({ role: "assistant", content: response.data.choices[0].message.content });
 
-      setChat(newResponse);
+          setChat(newResponse);
+          setPrompt("");
+        });
     } catch (error) {
       console.error(error);
     }
@@ -61,22 +71,27 @@ const ChatGPT = ({ secret }) => {
             className="block mr-2 p-2.5 w-full text-sm text-gray-900 bg-white rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             placeholder="Your message..."
           />
-
-          <button
-            type="submit"
-            className="inline-flex justify-center p-2 text-blue-600 rounded-full cursor-pointer hover:bg-blue-100 dark:text-blue-500 dark:hover:bg-gray-600"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="25"
-              height="25"
-              fill="currentColor"
-              className="bi bi-arrow-right-circle-fill"
-              viewBox="0 0 16 16"
+          {loading ? (
+            <button className="inline-flex justify-center p-2 text-blue-600 rounded-full cursor-pointer hover:bg-blue-100 dark:text-blue-500 dark:hover:bg-gray-600">
+              <Oval width="24" strokeWidth={4} stroke="currentColor" />
+            </button>
+          ) : (
+            <button
+              type="submit"
+              className="inline-flex justify-center p-2 text-blue-600 rounded-full cursor-pointer hover:bg-blue-100 dark:text-blue-500 dark:hover:bg-gray-600"
             >
-              <path d="M8 0a8 8 0 1 1 0 16A8 8 0 0 1 8 0zM4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z" />
-            </svg>
-          </button>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="25"
+                height="25"
+                fill="currentColor"
+                className="bi bi-arrow-right-circle-fill"
+                viewBox="0 0 16 16"
+              >
+                <path d="M8 0a8 8 0 1 1 0 16A8 8 0 0 1 8 0zM4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z" />
+              </svg>
+            </button>
+          )}
         </div>
       </form>
     </div>
