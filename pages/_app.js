1b14ad7ee0d58c3bef60407729bd8df9108aa705
Your name here <elia.orsini@hotmail.com>
2023-03-06 19:08:18 +0000

    fix: prevent zooming on iphone

--- pages/_app.js
+++ pages/_app.js
@@ -8,6 +8,7 @@ function MyApp({ Component, pageProps }) {
     <Layout>
       <Head>
         <title>prompts for cognitive biases</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
         <meta name="Description" content="prompts for cognitive biases" />
       </Head>
 
