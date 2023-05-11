1e9606a763e28a0faccf8a50db6074bffef3e5d2
Your name here <elia.orsini@hotmail.com>
2023-05-11 16:22:12 +0100

    fix: change title and icon

--- pages/_app.js
+++ pages/_app.js
@@ -6,9 +6,10 @@ function MyApp({ Component, pageProps }) {
   return (
     <Layout>
       <Head>
-        <title>prompts for cognitive biases</title>
+        <title>lazyLLM</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
-        <meta name="Description" content="prompts for cognitive biases" />
+        <meta name="Description" content="lazyLLM - tooling for prompt testing on language models" />
+        <link rel="icon" href="/lazyLLMlogo.png" />
       </Head>
 
       <Component {...pageProps} />
